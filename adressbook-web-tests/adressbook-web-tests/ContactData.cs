eee8c57ccbb4b2a9d9f789c0d8fc53d3bc722148
hatshepsutb <bykova.olga93@gmai.com>
2019-10-03 21:21:52 +0300

    Задание №4 Создан тест для контактов и  добавле класс для хранения свойств контактов

---
+++ adressbook-web-tests/adressbook-web-tests/ContactData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAdressbookTests
+{
+    class ContactData
+    {
+        private string firstName;
+        private string lastName;
+
+        public ContactData(string firstName)
+        {
+            this.firstName = firstName;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+    }
+}
